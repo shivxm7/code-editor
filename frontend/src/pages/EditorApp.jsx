28dc1bcada8a8be733748f00e6445aa0db627aeb
Shivam Sehgal <coderrshivam@gmail.com>
2024-12-16 19:53:56 +0530

    Adding expand full screen functanality on code editor

--- frontend/src/pages/EditorApp.jsx
+++ frontend/src/pages/EditorApp.jsx
@@ -6,6 +6,7 @@ import { AiOutlineExpandAlt } from "react-icons/ai";
 
 const EditorApp = () => {
   const [islightMode, setIsLightMode] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const changeTheme = () =>{
     if(islightMode){
@@ -22,7 +23,7 @@ const EditorApp = () => {
         <EditorNavbar/>
 
         <div className="flex">
-            <div className="left w-[50%]">
+            <div className={`left w-[${isExpanded ? "100%" : "50%"}]`}>
                 <div className="tabs flex items-center justify-between gap-2 w-full bg-[#1A1919] h-[50px] px-[40px]">
                   <div className='flex items-center gap-2'>
                     <div className='tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md'>index.html</div>
@@ -32,13 +33,13 @@ const EditorApp = () => {
 
                   <div className='flex items-center gap-2'>
                     <i className='features text-[20px] cursor-pointer' onClick={changeTheme}><MdLightMode /></i>
-                    <i className='features text-[20px] cursor-pointer'><AiOutlineExpandAlt /></i>
+                    <i className='features text-[20px] cursor-pointer' onClick={()=>(setIsExpanded(!isExpanded))}><AiOutlineExpandAlt /></i>
                   </div>
 
                 </div>
                 <Editor height="83vh" theme={islightMode ? "vs-light" : "vs-dark"}  defaultLanguage="javascript" defaultValue="// some comment" />
             </div>
-            <iframe id='output' className='w-[50%] min-h-[89vh] bg-[#fff] text-black'>iframe</iframe>
+            <iframe id='output' className={`w-[${isExpanded ? "0%" : "50%"}] ${isExpanded ? "hidden" : ""} min-h-[89vh] bg-[#fff] text-black`}>iframe</iframe>
         </div>
     </>
   )
