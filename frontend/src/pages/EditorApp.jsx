ff47c7a682ba44e23a00b13e354f286e6f7101a0
Shivam Sehgal <coderrshivam@gmail.com>
2024-12-17 22:16:18 +0530

    Fixing editor

--- frontend/src/pages/EditorApp.jsx
+++ frontend/src/pages/EditorApp.jsx
@@ -1,48 +1,145 @@
-import React, { useState } from 'react'
-import EditorNavbar from '../components/EditorNavbar'
-import Editor from '@monaco-editor/react';
+import React, { useState, useEffect } from "react";
+import EditorNavbar from "../components/EditorNavbar";
+import Editor from "@monaco-editor/react";
 import { MdLightMode } from "react-icons/md";
 import { AiOutlineExpandAlt } from "react-icons/ai";
 
 const EditorApp = () => {
   const [islightMode, setIsLightMode] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
+  const [tab, setTab] = useState("html");
+  const [htmlCode, setHtmlCode] = useState("<h1>Hello World!</h1>");
+  const [cssCode, setCssCode] = useState("body { background-color: #f4f4f4; }");
+  const [jsCode, setJsCode] = useState("console.log('Hello world');");
 
-  const changeTheme = () =>{
-    if(islightMode){
+  const changeTheme = () => {
+    if (islightMode) {
       document.body.classList.remove("lightMode");
       setIsLightMode(false);
-    }
-    else{
+    } else {
       document.body.classList.add("lightMode");
       setIsLightMode(true);
     }
-  }
+  };
+
+  const run = () => {
+    const html = htmlCode;
+    const css = `<style>${cssCode}</style>`;
+    const js = `<script>${jsCode}</script>`;
+
+    const iframe = document.getElementById("iframe");
+    iframe.srcdoc = html + css + js;
+  };
+
+  useEffect(() => {
+    run();
+  }, [htmlCode, cssCode, jsCode]);
+
   return (
     <>
-        <EditorNavbar/>
+      <EditorNavbar />
 
-        <div className="flex">
-            <div className={`left w-[${isExpanded ? "100%" : "50%"}]`}>
-                <div className="tabs flex items-center justify-between gap-2 w-full bg-[#1A1919] h-[50px] px-[40px]">
-                  <div className='flex items-center gap-2'>
-                    <div className='tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md'>index.html</div>
-                    <div className='tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md'>style.css</div>
-                    <div className='tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md'>script.js</div>
-                  </div>
-
-                  <div className='flex items-center gap-2'>
-                    <i className='features text-[20px] cursor-pointer' onClick={changeTheme}><MdLightMode /></i>
-                    <i className='features text-[20px] cursor-pointer' onClick={()=>(setIsExpanded(!isExpanded))}><AiOutlineExpandAlt /></i>
-                  </div>
-
-                </div>
-                <Editor height="83vh" theme={islightMode ? "vs-light" : "vs-dark"}  defaultLanguage="javascript" defaultValue="// some comment" />
+      <div className="flex">
+        <div className={`left w-[${isExpanded ? "100%" : "50%"}]`}>
+          <div className="tabs flex items-center justify-between gap-2 w-full bg-[#1A1919] h-[50px] px-[40px]">
+            <div className="flex items-center gap-2">
+              <div
+                onClick={() => {
+                  setTab("html");
+                }}
+                className="cursor-pointer tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md"
+              >
+                index.html
+              </div>
+              <div
+                onClick={() => {
+                  setTab("css");
+                }}
+                className="cursor-pointer tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md"
+              >
+                style.css
+              </div>
+              <div
+                onClick={() => {
+                  setTab("js");
+                }}
+                className="cursor-pointer tab p-[6px] bg-[#1E1E1E] px-[10px] text-[15px] rounded-md"
+              >
+                script.js
+              </div>
             </div>
-            <iframe id='output' className={`w-[${isExpanded ? "0%" : "50%"}] ${isExpanded ? "hidden" : ""} min-h-[89vh] bg-[#fff] text-black`}>iframe</iframe>
-        </div>
-    </>
-  )
-}
 
-export default EditorApp
+            <div className="flex items-center gap-2">
+              <i
+                className="features text-[20px] cursor-pointer"
+                onClick={changeTheme}
+              >
+                <MdLightMode />
+              </i>
+              <i
+                className="features text-[20px] cursor-pointer"
+                onClick={() => {
+                  setIsExpanded(!isExpanded);
+                }}
+              >
+                <AiOutlineExpandAlt />
+              </i>
+            </div>
+          </div>
+          {/*  Tab Functanality  */}
+
+          {tab == "html" ? (
+            <>
+              <Editor
+                onChange={(e) => {
+                  setHtmlCode(e || "");
+                  run();
+                }}
+                height="83vh"
+                theme={islightMode ? "vs-light" : "vs-dark"}
+                language="html"
+                value={htmlCode}
+              />
+            </>
+          ) : tab == "css" ? (
+            <>
+              <Editor
+                onChange={(e) => {
+                  setCssCode(e || "");
+                  run();
+                }}
+                height="83vh"
+                theme={islightMode ? "vs-light" : "vs-dark"}
+                language="css"
+                value={cssCode}
+              />
+            </>
+          ) : (
+            <>
+              <Editor
+                onChange={(e) => {
+                  setJsCode(e || "");
+                  run();
+                }}
+                height="83vh"
+                theme={islightMode ? "vs-light" : "vs-dark"}
+                language="js"
+                value={jsCode}
+              />
+            </>
+          )}
+        </div>
+        <iframe
+          id="iframe"
+          className={`w-[${isExpanded ? "0%" : "50%"}] ${
+            isExpanded ? "hidden" : ""
+          } min-h-[89vh] bg-[#fff] text-black`}
+        >
+          iframe
+        </iframe>
+      </div>
+    </>
+  );
+};
+
+export default EditorApp;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react'
-import logo from "../images/logo.png"
-import image from "../images/authPageSide.png"
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import logo from "../images/logo.png";
+import image from "../images/authPageSide.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [pwd, setPwd] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
-    const submitForm = () => {
-        e.preventDefault();
-    }
+  const submitForm = () => {
+    e.preventDefault();
+  };
   return (
     <>
-    <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
+      <div className="container w-screen min-h-screen flex items-center justify-between pl-[100px]">
         <div className="left w-[40%]">
-            <img className='w-[200px]' src={logo} alt="" />
-            <form onSubmit={submitForm} className='w-full mt-[60px]' action="">
-                <div className="inputbox">
-                    <input required onChange={(e) => {setEmail(e.target.value)}} value={email} type="text" placeholder='Email' />
-                </div>
+          <img className="w-[200px]" src={logo} alt="" />
+          <form onSubmit={submitForm} className="w-full mt-[60px]" action="">
+            <div className="inputbox">
+              <input
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                value={email}
+                type="text"
+                placeholder="Email"
+              />
+            </div>
 
-                <div className="inputbox">
-                    <input required onChange={(e) => {setPwd(e.target.value)}} value={pwd} type="text" placeholder='Password' />
-                </div>
+            <div className="inputbox">
+              <input
+                required
+                onChange={(e) => {
+                  setPwd(e.target.value);
+                }}
+                value={pwd}
+                type="text"
+                placeholder="Password"
+              />
+            </div>
 
-                <p className='text-[gray]'>Dont have an account <Link to="/Signup" className="text-[#00AEEF]">Sign Up</Link> </p>
+            <p className="text-[gray]">
+              Dont have an account{" "}
+              <Link to="/Signup" className="text-[#00AEEF]">
+                Sign Up
+              </Link>{" "}
+            </p>
 
-                <button className='btnBlue w-full mt-[20px]'>Login</button>
-            </form>
+            <button className="btnBlue w-full mt-[20px]">Login</button>
+          </form>
         </div>
         <div className="right w-[55%]">
-            <img className='h-[100vh] w-[100%] object-cover' src={image} alt="" />
+          <img className="h-[100vh] w-[100%] object-cover" src={image} alt="" />
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
